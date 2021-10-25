1a48a63850ab46b981a5d6f3f04753eeb2df1469
Towsif-Refat <towsifrifat@gmail.com>
2021-10-26 00:20:33 +0600

    Escrow System Using Blockchain Technology

---
+++ api/app.js
@@ -0,0 +1,382 @@
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const FabricCAServices = require('fabric-ca-client');
+const path = require('path');
+const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('../../fabric-samples/test-application/javascript/CAUtil.js');
+const { buildCCPOrg1, buildWallet } = require('../../fabric-samples/test-application/javascript/AppUtil.js');
+const { raw } = require('body-parser');
+
+const channelName = 'escrow';
+const chaincodeName = 'escrow17';
+const mspOrg1 = 'Org1MSP';
+const walletPath = path.join(__dirname, 'wallet');
+const org1UserId = '__app';
+
+function prettyJSONString(inputString) {
+	return JSON.stringify(JSON.parse(inputString), null, 2);
+}
+
+// sudo lsof -i :3000
+
+async function main() {
+	try {	
+		
+		const ccp = buildCCPOrg1();
+
+	
+		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+
+		
+		const wallet = await buildWallet(Wallets, walletPath);
+
+		
+		await enrollAdmin(caClient, wallet, mspOrg1);
+
+	
+		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
+
+		
+		const gateway = new Gateway();
+
+		try {
+		
+			await gateway.connect(ccp, {
+				wallet,
+				identity: org1UserId,
+				discovery: { enabled: true, asLocalhost: true } 
+			});
+
+			
+			const network = await gateway.getNetwork(channelName);
+
+			
+			const contract = network.getContract(chaincodeName);
+			
+			//create server
+			const express=require('express');
+			// var bodyParser = require('body-parser');
+			// const cookieParser = require('cookie-parser');
+			// const fileUpload = require('express-fileupload');
+			const path = require('path');
+			const crypto = require('crypto');
+            const fs = require('fs');
+			const util = require('util');
+			var cors = require('cors');
+			
+
+
+			let app=express();
+			const PORT=3000;
+
+
+			app.use(cors(
+
+			))
+
+			app.use(express.urlencoded({ extended: false }));
+			app.use(express.json());
+			app.use(express.static('public'));
+
+
+
+			app.get('/',function(req,res)
+			{
+				res.send('Welcome To Escrow!');
+			});
+
+			app.post('/createOrder',async function(req,res)
+			{
+				const {key, sellerId, buyerId, orderDeliveryDate} = req.body;
+				
+				try {
+					let result = await contract.evaluateTransaction(
+						'createOrder',
+						key,
+						sellerId,
+						buyerId,
+						orderDeliveryDate
+					);
+
+					await contract.submitTransaction(	
+						'createOrder',
+						key,
+						sellerId,
+						buyerId,
+						orderDeliveryDate
+					);
+					
+					res.send(result.toString());
+	
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			async function generateOTP() {
+          
+				var digits = '0123456789';
+				let OTP = '';
+				for (let i = 0; i < 4; i++ ) {
+					OTP += digits[Math.floor(Math.random() * 10)];
+				}
+				return OTP;
+			}
+			
+			async function getCurrentTime() {
+				let today = new Date();
+				let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+				let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+				let currentTime = date+' '+time;
+				return currentTime;
+			}
+
+			app.post('/depositBuyer',async function(req,res)
+			{
+				const {key, buyerId, sellerId, orderId, depositTransactionId, depositPaymentAmount} = req.body;
+
+				let fundReleaseKey = await generateOTP();
+				let depositTime = await getCurrentTime();
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'depositBuyer',
+						key,
+						buyerId,
+						sellerId,
+						orderId,
+						depositTransactionId,
+						depositPaymentAmount,
+						depositTime,
+						fundReleaseKey
+						);
+
+					await contract.submitTransaction(	
+						'depositBuyer',
+						key,
+						buyerId,
+						sellerId,
+						orderId,
+						depositTransactionId,
+						depositPaymentAmount,
+						depositTime,
+						fundReleaseKey
+					);
+					
+					res.send(result.toString());
+	
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			app.post('/depositSeller',async function(req,res){
+				const {key, sellerId, buyerId, orderId, depositTransactionId, depositPaymentAmount} = req.body;
+
+				let depositTime = await getCurrentTime();
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'depositSeller',
+						key,
+						sellerId,
+						buyerId,
+						orderId,
+						depositTransactionId,
+						depositPaymentAmount,
+						depositTime
+					);
+
+					await contract.submitTransaction(	
+						'depositSeller',
+						key,
+						sellerId,
+						buyerId,
+						orderId,
+						depositTransactionId,
+						depositPaymentAmount,
+						depositTime
+					);
+					
+					res.send(result.toString());
+	
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+			
+			app.post('/updateOrderStatus',async function(req,res){
+				const {key, newOrderStatus} = req.body;
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'updateOrderStatus',
+						key,
+						newOrderStatus
+						);
+
+					await contract.submitTransaction(	
+						'updateOrderStatus',
+						key,
+						newOrderStatus
+					);
+					
+					res.send(result.toString());
+
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			app.post('/createDeliveryAgent',async function(req,res){
+				const {key} = req.body;
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'createDeliveryAgent',
+						key
+						);
+
+					await contract.submitTransaction(	
+						'createDeliveryAgent',
+						key
+					);
+					
+					res.send(result.toString());
+
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			app.post('/assignDeliveryAgent',async function(req,res){
+				const {key, orderId} = req.body;
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'assignDeliveryAgent',
+						key,
+						orderId
+						);
+
+					await contract.submitTransaction(	
+						'assignDeliveryAgent',
+						key,
+						orderId
+					);
+					
+					res.send(result.toString());
+
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			app.post('/cancelOrder',async function(req,res){
+				const {key} = req.body;
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'cancelOrder',
+						key
+						);
+
+					await contract.submitTransaction(	
+						'cancelOrder',
+						key
+					);
+					
+					res.send(result.toString());
+
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			app.post('/releaseFund',async function(req,res){
+				const {key, depositTransactionId, fundReleaseKey} = req.body;
+
+				console.log(key,depositTransactionId,fundReleaseKey);
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'releaseFund',
+						key,
+						depositTransactionId,
+						fundReleaseKey
+						);
+
+					await contract.submitTransaction(	
+						'releaseFund',
+						key,
+						depositTransactionId,
+						fundReleaseKey
+					);
+					
+
+					res.send(result.toString());
+
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			app.post('/getOrderStatus',async function(req,res){
+				const {key} = req.body;
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'getOrderStatus',
+						key
+						);
+
+					await contract.submitTransaction(	
+						'getOrderStatus',
+						key
+					);
+					
+					res.send(result.toString());
+
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			app.post('/getBuyersReleaseFundKey',async function(req,res){
+				const {buyerDepositId} = req.body;
+
+				try {
+					let result = await contract.evaluateTransaction(
+						'getBuyersReleaseFundKey',
+						buyerDepositId
+						);
+
+					await contract.submitTransaction(	
+						'getBuyersReleaseFundKey',
+						buyerDepositId
+					);
+					
+					res.send(result.toString());
+
+				} catch (error) {
+					res.status(400).send(error.toString());
+				}
+			});
+
+			var server=app.listen(3000,function() {
+			console.log(`server listening on Port: ${PORT}`);
+		});
+
+
+	} finally {
+
+	}
+			
+		// gateway.disconnect();
+		
+ 	}catch (error) {
+		console.error(`******** FAILED to run the application: ${error}`);
+	}
+}
+
+main();
---
+++ chaincode/lib/escrow.js
@@ -0,0 +1,204 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class Escrow extends Contract {
+
+    async createOrder(ctx, key, sellerId, buyerId, OrderDeliveryDate){
+
+        const order = {
+            Key : key,
+            SellerId : sellerId,
+            BuyerId : buyerId,
+            OrderStatus : 'order created',
+            OrderDeliveryDate : OrderDeliveryDate,
+            IsOrderDeliveried : 'false',
+            IsOrderCancelled : 'false',
+            AgentId : 'not Selected Yet',
+        };
+
+        await ctx.stub.putState(key, Buffer.from(JSON.stringify(order)));
+        return JSON.stringify(order);
+    }
+
+    async depositBuyer(ctx, key, buyerId, sellerId, orderId, depositTransactionId, depositPaymentAmount, depositTime, fundReleaseKey) {
+
+        const buyerDeposit = {
+            Key: key,
+            BuyerId: buyerId,
+            SellerId: sellerId,
+            OrderId: orderId,
+            DepositTransactionId: depositTransactionId,
+            DepositPaymentAmount: depositPaymentAmount,
+            DepositTime: depositTime,
+            FundReleaseKey : fundReleaseKey,
+        };
+
+        await ctx.stub.putState(key, Buffer.from(JSON.stringify(buyerDeposit)));
+        return JSON.stringify(buyerDeposit);
+    }
+
+    async depositSeller(ctx, key, sellerId, buyerId, orderId, depositTransactionId, depositPaymentAmount, depositTime){
+        const sellerDeposit = {
+            Key : key,
+            SellerId : sellerId, 
+            BuyerId : buyerId,
+            OrderId : orderId, 
+            DepositTransactionId : depositTransactionId,
+            DepositPaymentAmount : depositPaymentAmount,
+            DepositTime : depositTime,
+        };
+
+        let sellerDepositStatus = await ctx.stub.putState(key, Buffer.from(JSON.stringify(sellerDeposit)));
+        
+        if(sellerDepositStatus){
+            await this.updateOrderStatus(ctx, orderId, "Seller Deposited, Order Confirmed");
+        }
+
+        return JSON.stringify(sellerDeposit);
+    }
+
+    async updateOrderStatus(ctx, key, newOrderStatus){
+
+        const fileJSON = await ctx.stub.getState(key);
+
+        if (!fileJSON || fileJSON.length === 0) {
+            throw new Error(`The order ${key} does not exist`);
+        }
+
+        let status = JSON.parse(fileJSON.toString());
+        status.OrderStatus = newOrderStatus;
+
+        await ctx.stub.putState(key, Buffer.from(JSON.stringify(status)));
+        return JSON.stringify(status);
+    }
+
+    async updateOrderAgentId(ctx, key, assignedAgentId){
+
+        const fileJSON = await ctx.stub.getState(key);
+
+        if (!fileJSON || fileJSON.length === 0) {
+            throw new Error(`The order ${key} does not exist`);
+        }
+
+        let agentId = JSON.parse(fileJSON.toString());
+        agentId.AgentId = assignedAgentId;
+
+        await ctx.stub.putState(key, Buffer.from(JSON.stringify(agentId)));
+        return JSON.stringify(agentId);
+    }
+
+    async createDeliveryAgent(ctx, key){
+        const agent = {
+            Key : key,
+            IsAgentSelected : 'false',
+        };
+
+        await ctx.stub.putState(key, Buffer.from(JSON.stringify(agent)));
+        return JSON.stringify(agent);
+    }
+
+    async assignDeliveryAgent(ctx, key, orderId){
+        
+        let status = await this.updateOrderAgentId(ctx, orderId, key);
+        let status_ = await this.updateOrderStatus(ctx, orderId, "Order Shipped");
+      
+
+        
+
+        if(status || status_){
+            const fileJSON = await ctx.stub.getState(key);
+            if (!fileJSON || fileJSON.length === 0) {
+                throw new Error('The agent does not exist');
+            }
+            
+            let agentStatus = JSON.parse(fileJSON.toString());
+            agentStatus.IsAgentSelected = 'true';
+
+            await ctx.stub.putState(key, Buffer.from(JSON.stringify(agentStatus)));
+
+            return JSON.stringify(agentStatus);
+        }
+    }
+
+    // cancel order
+
+    async cancelOrder(ctx, key){
+
+        await this.updateOrderStatus(ctx, key, "Order Cancelled");
+
+        const fileJSON = await ctx.stub.getState(key);
+
+        if (!fileJSON || fileJSON.length === 0) {
+            throw new Error(`The order ${key} does not exist`);
+        }
+
+        let orderCancelStatus = JSON.parse(fileJSON.toString());
+        orderCancelStatus.IsOrderCancelled = 'true';
+
+        await ctx.stub.putState(key, Buffer.from(JSON.stringify(orderCancelStatus)));
+        return JSON.stringify(orderCancelStatus);
+
+    }
+
+    async getBuyersReleaseFundKey(ctx, key){
+        const fileJSON = await ctx.stub.getState(key);
+        if (!fileJSON || fileJSON.length === 0) {
+            throw new Error(`The order ${key} does not exist`);
+        }
+
+        let releaseFund = JSON.parse(fileJSON.toString());
+        let releaseFundKey = releaseFund.FundReleaseKey;
+
+        return releaseFundKey;
+    }
+
+    async releaseFund(ctx, key, depositTransactionId, fundReleaseKey){
+
+        
+        let releaseKey = await this.getBuyersReleaseFundKey(ctx, depositTransactionId);
+        if(releaseKey){
+            if(releaseKey === fundReleaseKey){
+                const fileJSON = await ctx.stub.getState(key);
+    
+                if (!fileJSON || fileJSON.length === 0) {
+                    throw new Error('The order does not exist');
+                }
+                
+                let currentOrderStatus = JSON.parse(fileJSON.toString());
+                //call payment processing api here
+                currentOrderStatus.IsOrderDeliveried = 'true';
+                
+                await ctx.stub.putState(key, Buffer.from(JSON.stringify(currentOrderStatus)));
+                return JSON.stringify(currentOrderStatus);
+            }
+        }
+
+    }
+
+    async getOrderStatus(ctx, key){
+
+        const fileJSON = await ctx.stub.getState(key);
+        if (!fileJSON || fileJSON.length === 0) {
+            throw new Error(`The order ${key} does not exist`);
+        }
+
+        let order = JSON.parse(fileJSON.toString());
+        let orderStatus = order.OrderStatus;
+
+        return JSON.stringify(orderStatus);
+
+    }
+
+
+}
+
+// ./network.sh deployCC -c escrow -ccn escrow1 -ccp ../../escrow/chaincode/ -ccl javascript
+
+module.exports = Escrow;
